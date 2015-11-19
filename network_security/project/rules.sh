34abcca7ae92e35b6e9a472ec722243d7ef7c4fd
Samuel Monroe <spat.monroe@gmail.com>
2015-11-19 12:50:22 +0100

    Avancement sur projet sécurité pratique

---
+++ network_security/project/rules.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################
+# REGLES DE F3 #
+################
+
+R1=192.168.3.10
+SSH=192.168.1.10
+PROCESSOR=192.168.4
+FTP=92.168.1.12
+# Default DROP
+
+	iptables -P INPUT DROP
+	iptables -P OUTPUT DROP
+	iptables -P FORWARD DROP
+
+# Processor - accessible uniquement par R1 et SSH - en SSH
+
+	# Autorise forwarding d'une connexion NEW ou ETABLIE en ssh depuis R1 vers Processor
+	iptables -A FORWARD -p tcp -s $R1 -d $PROCESSOR --dport ssh --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -p tcp -s $PROCESSOR -d $R1 --sport ssh --state ESTABLISHED -j ACCEPT
+
+	# Autorise le forwarding d'une connexion NEW ou ETABLIE en ssh depuis SSH vers Processor
+	# et inversément pour une établie pour les réponses de SSH
+	iptables -A FORWARD -p tcp -s $SSH -d $PROCESSOR --dport ssh --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -p tcp -s $PROCESSOR -d $SSH --sport ssh --state ESTABLISHED -j ACCEPT
+
+# Processor - Peut communiquer avec le FTP pour obtenir les fichiers
+
+	iptables -A FORWARD -p tcp -s $PROCESSOR -d $FTP --sport 21:22 --dport 21:22 --state NEW, ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -p tcp -s $FTP -d $PROCESSOR --sport 21:22 --dport 21:22 --state ESTABLISHED -j ACCEPT
